eb96af6aeb971c38a951d8168ffaa9f7fba68e0b
Kyi <thazin31086@gmail.com>
2016-12-19 21:30:32 +1100

    Commit

---
+++ Jasmine.POS.SQL/Jasmine.POS.SQL/Role.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Role]
+(
+	[RoleID] INT NOT NULL PRIMARY KEY, 
+    [Description] VARCHAR(300) NULL 
+)
---
+++ Jasmine.POS.SQL/Jasmine.POS.SQL/User.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [UserName] VARCHAR(50) NULL, 
+    [FirstName] NCHAR(10) NULL, 
+    [LastName] NCHAR(10) NULL, 
+    [RoleID] INT NULL, 
+    [TFN] VARCHAR(50) NULL, 
+    [mobile] VARCHAR(50) NULL, 
+    [address] VARBINARY(300) NULL, 
+    CONSTRAINT [FK_User_ToRole] FOREIGN KEY ([RoleID]) REFERENCES [Role]([RoleID]), 
+	
+)
