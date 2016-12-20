7ab0f0ff8aef2378993c88b0a4c3edf7530c3165
Jasmine Aung <thazin31086@gmail.com>
2016-12-20 16:57:14 +1100

    Test Class

---
+++ Jasmine.POS/Jasmine.POS.Test/Repository/UserRepositoryTests.cs
@@ -0,0 +1,34 @@
+﻿using Jasmine.POS.Data.IRepository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Jasmine.POS.Data.Repository.Tests
+{
+    [TestClass()]
+    public class UserRepositoryTests
+    {
+        [TestMethod()]
+        public void GetAllUserTest()
+        {
+            //Assign 
+            int expected = 1;
+            int actual = 0;
+            //Act 
+            using (var JasDBContext = new JasminePOSDBContext())
+            {
+                //IUserRepository userrepo = new UserRepository(JasDBContext);
+                //var users = userrepo.GetAllUser();
+
+                //actual = users.Count();
+
+                IProductRepository repo = new ProductRepository(JasDBContext);
+                var products = repo.GetAllProducts();
+
+                actual = products.Count();
+            }
+
+            //Asset
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
